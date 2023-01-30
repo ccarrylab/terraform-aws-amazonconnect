01778d633e13ec2b7f20b7d21b511e58d899277b
ccarrylab81 <cohen@globo-tek.com>
2023-01-30 16:09:04 -0500

    ..

--- examples/complete/hours_of_operations.tf
+++ examples/complete/hours_of_operations.tf
@@ -61,11 +61,29 @@ locals {
   }
 }
 
-  ceosales = {
-    description = "HOOP for ceoSales"
-    time_zone   = local.time_zone
-    config = [
-      for w in local.ceosales_weekdays : {
+ceosales = {
+  description = "HOOP for ceoSales"
+  time_zone   = local.time_zone
+  config = [
+    for w in local.ceosales_weekdays : {
+      day = w
+      start_time = {
+        hours   = 8 # 8 AM
+        minutes = 0
+      }
+      end_time = {
+        hours   = 18 # 6 PM
+        minutes = 0
+      }
+    }
+  ]
+}
+ceosupport = {
+  description = "HOOP for CEOSupport"
+  time_zone   = local.time_zone
+  config = flatten([
+    [
+      for w in local.ceosupport_weekdays : {
         day = w
         start_time = {
           hours   = 8 # 8 AM
@@ -76,43 +94,24 @@ locals {
           minutes = 0
         }
       }
-    ]
-  }
-  ceosupport = {
-    description = "HOOP for CEOSupport"
-    time_zone   = local.time_zone
-    config = flatten([
-      [
-        for w in local.ceosupport_weekdays : {
+    ],
+    flatten([
+      for w in local.ceosupport_weekends : [
+        # Second loop, need two start/end times per day, with break in between
+        # 9 AM - 12PM, 1 PM - 5 PM
+        for t in [{ start = 9, end = 12 }, { start = 13, end = 17 }] : {
           day = w
           start_time = {
-            hours   = 8 # 8 AM
+            hours   = t.start
             minutes = 0
           }
           end_time = {
-            hours   = 18 # 6 PM
+            hours   = t.end
             minutes = 0
           }
         }
-      ],
-      flatten([
-        for w in local.ceosupport_weekends : [
-          # Second loop, need two start/end times per day, with break in between
-          # 9 AM - 12PM, 1 PM - 5 PM
-          for t in [{ start = 9, end = 12 }, { start = 13, end = 17 }] : {
-            day = w
-            start_time = {
-              hours   = t.start
-              minutes = 0
-            }
-            end_time = {
-              hours   = t.end
-              minutes = 0
-            }
-          }
-        ]
-      ])
-    ]),
-  }
+      ]
+    ])
+  ]),
 }
 
