624f6f197e46e7848b5e25476f5a9e215ba4a00f
ccarrylab81 <cohen@globo-tek.com>
2023-01-30 15:05:04 -0500

    ..

--- examples/complete/contact_flows.tf
+++ examples/complete/contact_flows.tf
@@ -77,3 +77,17 @@ resource "aws_connect_contact_flow_module" "gts" {
     "Method"      = "Create"
   }
 }
+
+resource "aws_connect_contact_flow_module" "example" {
+  instance_id  = "a7952ceb-7e2a-4b8a-bc36-ae21e64d65e9"
+  name         = "Example"
+  description  = "Example Contact Flow Module Description"
+  filename     = "contact_flow_module.json"
+  content_hash = filebase64sha256("contact_flow_module.json")
+
+  tags = {
+    "Name"        = "Example Contact Flow Module",
+    "Application" = "Terraform",
+    "Method"      = "Create"
+  }
+}
