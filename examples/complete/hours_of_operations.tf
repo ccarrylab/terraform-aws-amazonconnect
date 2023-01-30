dcc70a6bf536f592295e3a139a5828b2989e577c
ccarrylab81 <cohen@globo-tek.com>
2023-01-30 16:05:05 -0500

    ..

--- examples/complete/hours_of_operations.tf
+++ examples/complete/hours_of_operations.tf
@@ -22,24 +22,6 @@ locals {
         }
       ]
     }
-    hours_of_operations = {
-    ceosales = {
-      description = "HOOP for CEOSales"
-      time_zone   = local.time_zone
-      config = [
-        for w in local.ceosales_weekdays : {
-          day = w
-          start_time = {
-            hours   = 8 # 8 AM
-            minutes = 0
-          }
-          end_time = {
-            hours   = 18 # 6 PM
-            minutes = 0
-          }
-        }
-      ]
-    hours_of_operations = {
     support = {
       description = "HOOP for Support"
       time_zone   = local.time_zone
@@ -56,10 +38,49 @@ locals {
               minutes = 0
             }
           }
-        ]
-        hours_of_operations = {
-      ceosupport = {
-      description = "HOOP for ceoSupport"
+        ],
+        flatten([
+          for w in local.support_weekends : [
+            # Second loop, need two start/end times per day, with break in between
+            # 9 AM - 12PM, 1 PM - 5 PM
+            for t in [{ start = 9, end = 12 }, { start = 13, end = 17 }] : {
+              day = w
+              start_time = {
+                hours   = t.start
+                minutes = 0
+              }
+              end_time = {
+                hours   = t.end
+                minutes = 0
+              }
+            }
+          ]
+        ])
+      ])
+    },
+  }
+}
+
+ hours_of_operations = {
+    ceosales = {
+      description = "HOOP for ceoSales"
+      time_zone   = local.time_zone
+      config = [
+        for w in local.ceosales_weekdays : {
+          day = w
+          start_time = {
+            hours   = 8 # 8 AM
+            minutes = 0
+          }
+          end_time = {
+            hours   = 18 # 6 PM
+            minutes = 0
+          }
+        }
+      ]
+    }
+    ceosupport = {
+      description = "HOOP for CEOSupport"
       time_zone   = local.time_zone
       config = flatten([
         [
@@ -76,7 +97,7 @@ locals {
           }
         ],
         flatten([
-          for w in local.support_weekends : [
+          for w in local.ceosupport_weekends : [
             # Second loop, need two start/end times per day, with break in between
             # 9 AM - 12PM, 1 PM - 5 PM
             for t in [{ start = 9, end = 12 }, { start = 13, end = 17 }] : {
