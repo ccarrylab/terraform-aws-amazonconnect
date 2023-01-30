9e9fe9e641d41a3a2f0d172a73f2bb0e2f739b78
ccarrylab81 <cohen@globo-tek.com>
2023-01-30 13:43:31 -0500

    ..

--- examples/complete/contact_flows.tf
+++ examples/complete/contact_flows.tf
@@ -23,7 +23,7 @@ locals {
   }
 }
 
-resource "aws_connect_contact_flow_module" "example" {
+resource "aws_connect_contact_flow_module" "gts" {
   instance_id = "a7952ceb-7e2a-4b8a-bc36-ae21e64d65e9"
   name        = "GTS Example"
   description = "Example Contact Flow Module Description"
